ef93f4980f9c4c7115ec91975182778f3261f617
Matthew Desabelle <71741074+dain-potato@users.noreply.github.com>
2024-10-28 14:46:45 +0800

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -8,9 +8,9 @@ import joblib
 # TRAIN THE MODEL EVERY 3 MONTHS OR 6 MONTHS OR YEAR (DEPENDS)
 # Step 1: Connect to the MySQL database
 db_connection = mysql.connector.connect(
-    host='localhost',  # XAMPP default host
+    host='165.22.59.23',  # XAMPP default host
     user='root',  # XAMPP default user
-    password='',  # Leave password blank unless you have set one
+    password='password',  # Leave password blank unless you have set one
     database='fixedasset'  # Your database name
 )
 
